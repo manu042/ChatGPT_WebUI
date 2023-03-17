dc830d9f7e5ea68bab83586c867f480f029cc79d
manu042 <25594761+manu042@users.noreply.github.com>
2023-03-17 21:09:54 +0100

    Change url

--- chatgpt_api_ui/urls.py
+++ chatgpt_api_ui/urls.py
@@ -6,6 +6,6 @@ app_name = 'chatgpt_api_ui'
 urlpatterns = [
     path('', views.chat_view, name="chat_ui"),
     re_path(r'^(?P<pk>\d+)/$', views.chat_view, name="chat_pk"),
-    path("chat_completion/", views.chat_completion),
     path("new_chat/", views.create_chat, name="new_chat"),
+    path("api/chat_completion/", views.chat_completion_view),
 ]
