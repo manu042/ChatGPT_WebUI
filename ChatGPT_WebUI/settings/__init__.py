053c70e6447dbe790d24017d2601feb205e2167b
manu042 <25594761+manu042@users.noreply.github.com>
2023-03-18 21:31:05 +0100

    Check if api key is provided.

--- ChatGPT_WebUI/settings/__init__.py
+++ ChatGPT_WebUI/settings/__init__.py
@@ -1,5 +1,10 @@
 from ChatGPT_WebUI.settings.base import *
 
+try:
+    CHAT_GPT_API_KEY = os.environ["CHAT_GPT_API_KEY"]
+except KeyError:
+    raise KeyError("OpenAi API key missing.")
+
 
 if os.environ.get("DEVELOPMENT"):
     from ChatGPT_WebUI.settings.development import *
